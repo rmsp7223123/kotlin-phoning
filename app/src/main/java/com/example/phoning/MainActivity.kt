a1fc6d84f7e67f8e967c71e5b80132ad5832b03f
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-09-09 20:28:52 +0900

    파베 추가, 메인 클릭시 웹 연결

--- app/src/main/java/com/example/phoning/MainActivity.kt
+++ app/src/main/java/com/example/phoning/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.phoning
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,10 +14,13 @@ class MainActivity : AppCompatActivity() {
     // val = 가변(값의 읽기와 쓰기가 모두 허용되는 변수)
 
     // 전역 변수로 바인딩 객체 선언
-    // ?  = null 을허용하는
+    // ?(nullable)  = null 을허용하는
     private var mBinding : ActivityMainBinding? = null
+    //           변수이름 :   타입  =
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
-    private val binding get() =  mBinding!!
+    private val binding get() =  mBinding!! // !! = 널안전연산자 ( 변수가 절대로 null일 수 없음)
+
+    private lateinit var intent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,11 +28,17 @@ class MainActivity : AppCompatActivity() {
         val hideActionBar = HideActionBar()
         hideActionBar.hideActionBar(this)
 
-        binding.relativeSettings.setOnClickListener(View.OnClickListener {
+        binding!!.relativeSettings.setOnClickListener(View.OnClickListener {
             // :: 참조
-            var intent = Intent(this, SettingActivity::class.java);
+            intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        })
+
+        binding!!.relativeClick.setOnClickListener(View.OnClickListener {
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://newjeans.kr/"))
             startActivity(intent)
         });
+
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/phoning/Setting/SettingActivity.kt
+++ app/src/main/java/com/example/phoning/Setting/SettingActivity.kt
@@ -8,15 +8,18 @@ import com.example.phoning.R
 import com.example.phoning.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
-
-    private var mBinding : ActivitySettingBinding? = null
-    private val binding get() = mBinding
+    // lateinit 나중에 초기화 // 그렇지 않으면 오류 발생
+    private lateinit var binding: ActivitySettingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        // 초기화
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val hideActionBar = HideActionBar()
         hideActionBar.hideActionBar(this)
-
+        binding!!.imgvBack.setOnClickListener{
+            finish()
+        }
     }
 }
