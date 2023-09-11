cf1822c0ba689ad7c404fe48303eb1675579dffb
rmsp7223123 <rmsp7223123@naver.com>
2023-09-11 16:35:28 +0900

    통화 메인 수정, 통화 커먼 수정

--- app/src/main/java/com/example/phoning/calls/CallsMainActivity.kt
+++ app/src/main/java/com/example/phoning/calls/CallsMainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.phoning.HideActionBar
 import com.example.phoning.databinding.ActivityCallsMainBinding
+import com.example.phoning.dto.CallsMainDTO
 
 class CallsMainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityCallsMainBinding
-
-    private var count : Int = 1
-
-    private var count2 : Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +18,18 @@ class CallsMainActivity : AppCompatActivity() {
         val hideActionBar = HideActionBar()
         hideActionBar.hideActionBar(this)
 
+//        val adapter = CallsMainAdapter()
+//        binding.recv.apply {
+//            layoutManager = LinearLayoutManager(this@CallsMainActivity)
+//            this.adapter = adapter
+//        }
+
         binding.imgvBack.setOnClickListener { finish() }
+
+    }
+
+    private fun callList() : ArrayList<CallsMainDTO> {
+        val list = ArrayList<CallsMainDTO>()
+        return list
     }
 }
--- app/src/main/java/com/example/phoning/common/CallsCommon.kt
+++ app/src/main/java/com/example/phoning/common/CallsCommon.kt
@@ -3,4 +3,6 @@ package com.example.phoning.common
 object CallsCommon {
 
     var IsCheck = booleanArrayOf(false, false, false, false, false, false, false, false)
+    var alarmCount : Int = 0
+    var callCount : Int = 0
 }
