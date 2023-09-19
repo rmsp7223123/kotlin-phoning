c2f17d90db34b620885bbbbdb1c52f8549ddd9aa
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-09-19 19:33:06 +0900

    채팅 어댑터 추가

--- app/src/main/java/com/example/phoning/dto/MessageMainDTO.kt
+++ app/src/main/java/com/example/phoning/dto/MessageMainDTO.kt
@@ -1,4 +1,4 @@
 package com.example.phoning.dto
 
-class MessageMainDTO (var imgRes : Int, var name : String, var content : String, var date : String);
+class MessageMainDTO (var imgRes : Int, var name : String, var content : String, var date : String, var isCheck : Boolean);
 
--- app/src/main/java/com/example/phoning/message/MessageChatAdapter.kt
+++ app/src/main/java/com/example/phoning/message/MessageChatAdapter.kt
@@ -1,9 +1,15 @@
 package com.example.phoning.message
 
 import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.phoning.R
 import com.example.phoning.databinding.ItemRecvMessageChatBinding
 import com.example.phoning.dto.MessageMainDTO
 import com.google.firebase.database.DatabaseReference
@@ -20,8 +26,62 @@ class MessageChatAdapter(private val list: ArrayList<MessageMainDTO>, private va
     };
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val chatDTO = list[position];
+        holder.binding.tvName.text = chatDTO.name
+        holder.binding.imgvMain.setImageResource(chatDTO.imgRes)
+
+        holder.binding.containerFrame.removeAllViews()
+        val tvMsg = TextView(context)
+        val tvTime = TextView(context)
+        val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val params2 = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        if (chatDTO.isCheck) {
+            holder.binding.containerFrame.layoutParams = params2
+            holder.binding.tvName.visibility = View.GONE
+            holder.binding.imgvMain.visibility = View.GONE
+            holder.binding.cVMain.visibility = View.GONE
+            params2.gravity = Gravity.END
+            tvMsg.text = chatDTO.content
+            tvMsg.setTextColor(Color.parseColor("#000000"))
+            tvMsg.textSize = 16f
+            tvMsg.setBackgroundResource(R.drawable.message_chat_me_background)
+            tvMsg.setPadding(30, 20, 70, 20)
+            tvMsg.maxWidth = 800
+            params.gravity = Gravity.BOTTOM or Gravity.END
+            params.setMargins(0, 0, 20, 0)
+            tvTime.layoutParams = params
+            tvTime.text = chatDTO.date
+            tvTime.setTextColor(Color.parseColor("#000000"))
+            tvTime.textSize = 12f
+            holder.binding.containerFrame.addView(tvTime)
+            holder.binding.containerFrame.addView(tvMsg)
+        } else {
+            tvMsg.text = chatDTO.content
+            tvMsg.setTextColor(Color.parseColor("#000000"))
+            tvMsg.textSize = 16f
+            tvMsg.setBackgroundResource(R.drawable.message_chat_background)
+            tvMsg.setPadding(30, 20, 70, 20)
+            tvMsg.maxWidth = 800
+            params.gravity = Gravity.BOTTOM or Gravity.START
+            tvTime.layoutParams = params
+            tvTime.text = chatDTO.date
+            tvTime.setTextColor(Color.parseColor("#000000"))
+            tvTime.textSize = 12f
+            holder.binding.containerFrame.addView(tvMsg)
+            holder.binding.containerFrame.addView(tvTime)
+        };
     };
 
     inner class ViewHolder(val binding: ItemRecvMessageChatBinding) : RecyclerView.ViewHolder(binding.root);
+
+    fun addData(chatDTO: MessageMainDTO) {
+        list.add(chatDTO);
+        notifyDataSetChanged();
+    };
+
+    fun removeData(chatDTO: MessageMainDTO) {
+        list.remove(chatDTO);
+        notifyDataSetChanged();
+    };
 }
